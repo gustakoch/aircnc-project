763dc5481fd67aece61656718a2a34e8b94849a4
Gustavo Koch <gustavok@outlook.com.br>
2019-10-08 19:25:09 -0300

    adicionado projeto mobile ao repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,15 @@
-import React, { useState } from "react";
-import api from './services/api';
+import React from "react";
 import "./App.css";
 
+import Routes from './routes';
 import logo from './assets/logo.svg';
 
 function App() {
-  const [ getEmail, setEmail ] = useState('');
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const response = await api.post('/sessions', { email: getEmail });
-    const { _id } = response.data;
-
-    localStorage.setItem('user_id', _id);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
-
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para sua empresa!
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL: (*)</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Informe seu melhor e-mail"
-            value={getEmail}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
